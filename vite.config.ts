77fdf0d5ef352df1864f9e404aafe1522931135a
Yasmim Freire <yasmimfreire2022@gmail.com>
2025-10-04 12:48:56 -0300

    Terceiro commit para pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from "path"
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
-  base: '/portfolioyf/', // 👈 nome do repositório
-  plugins: [react()]
+  base: "/portfolioyf/",
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src")
+    }
+  },
+  server: {
+    host: true,
+    port: 8080,
+    strictPort: true
+  }
 })
